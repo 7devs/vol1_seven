654ece667d217954e7c49f980dec71e1b3324672
Seven Yu <dofyyu@gmail.com>
2016-09-13 13:23:45 +0800

    add models

---
+++ homework1/models/album.js
@@ -0,0 +1,51 @@
+// 唱片数据
+module.exports = [
+    {
+        title: '私奔',   // 歌曲标题
+        class: '摇滚',   // 歌曲风格
+        length: 203,     // 歌曲时长（单位：秒）
+        singer: '郑钧'   // 演唱者
+    },
+    {
+        title: '无地自容',
+        class: '摇滚',
+        length: 127,
+        singer: '黑豹乐队'
+    },
+    {
+        title: '去大理',
+        class: '民谣',
+        length: 353,
+        singer: '黄渤'
+    },
+    {
+        title: '别让我心疼',
+        class: '流行',
+        length: 182,
+        singer: '郑中基'
+    },
+    {
+        title: '女儿情',
+        class: '民谣',
+        length: 92,
+        singer: '万晓利'
+    },
+    {
+        title: 'Don\'t Break My Heart',
+        class: '摇滚',
+        length: 158,
+        singer: '黑豹乐队'
+    },
+    {
+        title: '商品社会',
+        class: '摇滚',
+        length: 243,
+        singer: '郑钧'
+    },
+    {
+        title: '狐狸',
+        class: '民谣',
+        length: 369,
+        singer: '万晓利'
+    }
+];
---
+++ homework1/models/users.js
@@ -0,0 +1,59 @@
+// 用户数据
+module.exports = [
+    {
+        firstName: 'Python',     // 名
+        lastName: 'Do',          // 姓
+        age: '15',               // 年龄
+        sex: 'male',             // 性别（male 男，female 女）
+        company: 'GeekPark Inc.' // 公司（GeekPark Inc. | YoKeNeng ShangHai | YoKeNeng BeiJing）
+    },
+    {
+        firstName: 'PHP',
+        lastName: 'Re',
+        age: '35',
+        sex: 'female',
+        company: 'YoKeNeng ShangHai'
+    },
+    {
+        firstName: 'Ruby',
+        lastName: 'Mi',
+        age: '5',
+        sex: 'male',
+        company: 'YoKeNeng BeiJing'
+    },
+    {
+        firstName: 'C',
+        lastName: 'Sharp',
+        age: '7',
+        sex: 'male',
+        company: 'YoKeNeng ShangHai'
+    },
+    {
+        firstName: 'JavaScript',
+        lastName: 'Fa',
+        age: '15',
+        sex: 'female',
+        company: 'YoKeNeng BeiJing'
+    },
+    {
+        firstName: 'Basic',
+        lastName: 'Sol',
+        age: '28',
+        sex: 'male',
+        company: 'YoKeNeng BeiJing'
+    },
+    {
+        firstName: 'Java',
+        lastName: 'La',
+        age: '38',
+        sex: 'male',
+        company: 'GeekPark Inc.'
+    },
+    {
+        firstName: 'Go',
+        lastName: 'Ti',
+        age: '17',
+        sex: 'female',
+        company: 'YoKeNeng BeiJing'
+    }
+];
