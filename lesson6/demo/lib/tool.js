d7bdc1e561ef872bfc51d1363599b3cc475ca39d
Seven Yu <dofyyu@gmail.com>
2016-09-14 11:16:01 +0800

    添加 tool 私有包注释

--- lesson6/demo/lib/tool.js
+++ lesson6/demo/lib/tool.js
@@ -1,5 +1,8 @@
+/* 自定义包，一般放在项目目录下的 lib 文件夹中 */
 // 定义工具方法
 function printDate() {
+//------ ---------
+// 关键字   方法名
     var d = new Date();
     console.log(d.toString());
 }
@@ -10,6 +13,8 @@ function printLine() {
 
 // 对外开放接口
 module.exports.printDate = printDate;
+//------------ ---------   ---------
+//   固定写法   对外方法名    内部方法名
 module.exports.printLine = printLine;
 
 /*
