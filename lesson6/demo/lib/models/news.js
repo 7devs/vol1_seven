3d88702d7971fa396b18a0807c546e500fe56716
Seven Yu <dofyyu@gmail.com>
2016-09-14 11:16:26 +0800

    添加 models/news.js 注释

--- lesson6/demo/lib/models/news.js
+++ lesson6/demo/lib/models/news.js
@@ -1,22 +1,40 @@
+/* 定义数据对象（模拟数据） */
 module.exports = [
+//------------   -
+// 固定写法       中括号代表在定义一个数组，与最后一行的中括号是一对
+/* 数组第 1 个元素（开始），索引值为 0 */
     {
+//  -
+// 大括号在这里表示开始定义一个对象（Object）
+// 对象的属性或方法用键值对表示（key: value）
         title: 'news 1',
+//      -----  --------
+//       key    value
+//       键名     值
         content: 'new content 1',
         author: 'Author A'
     },
+/* 数组第 1 个元素（结束） */
+/* 数组第 2 个元素（开始），索引值为 1 */
     {
         title: 'news 2',
         content: 'new content 2',
         author: 'Author B'
     },
+/* 数组第 2 个元素（结束） */
+/* 数组第 3 个元素（开始），索引值为 2 */
     {
         title: 'news 3',
         content: 'new content 3',
         author: 'Author C'
     },
+/* 数组第 3 个元素（结束） */
+/* 数组第 4 个元素（开始），索引值为 3 */
     {
         title: 'news 4',
         content: 'new content 4',
         author: 'Author D'
     }
+/* 数组第 4 个元素（结束） */
 ];
+/* 整个数组定义完毕 */
